6078bf7ff6f068e1b313d62b124aa8e7068b5141
Siddhartha Haldar <sidhaldar98@gmail.com>
2023-09-20 15:41:11 -0700

    trigger name changed

--- testHttpFunction/function.js
+++
@@ -1,5 +0,0 @@
-const functions = require('@google-cloud/functions-framework');
-
-functions.http('helloHttp', (req, res) => {
-  res.send(`Hello there  ${req.query.name || req.body.name || 'World, hope its going well?'}!`);
-});
