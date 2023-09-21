ff69de68f2fae02c03072c02f553adf49e3c0533
Siddhartha Haldar <sidhaldar98@gmail.com>
2023-09-20 18:17:54 -0700

    Changed response

--- testHttpFunction/function.js
+++ testHttpFunction/function.js
@@ -1,5 +1,5 @@
 const functions = require('@google-cloud/functions-framework');
 
 functions.http('helloWorld', (req, res) => {
-  res.send(`Hello  there ${req.query.name || req.body.name || 'World, hope its going well?'}!`);
+  res.send(`Hello there ${req.query.name || req.body.name || 'World, hope its going well?'}!`);
 });
